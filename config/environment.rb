80f6908ce3683f2f0be9bd94abacbd45c3bcbb32
Ali Chihab <ali.chihab@hotmail.com>
2019-04-13 20:13:20 +0000

    storage issue

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,7 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
 ActionMailer::Base.smtp_settings = {
   :port           => ENV['MAILGUN_SMTP_PORT'],
   :address        => ENV['MAILGUN_SMTP_SERVER'],
